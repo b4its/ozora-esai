e39e4f323d9e55258fa9355752f7ff19fad5d56e
b4its <exynosfam@gmail.com>
2026-02-13 10:53:23 +0800

    fix: improve iot_services view, add authenticated, and synchronized to iot hardware

--- web_ozora/main/views.py
+++ web_ozora/main/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from rest_framework.authtoken.models import Token
 
-# Create your views here.
-
-def home (request):
-    return render(request,'index.html')
+@login_required # Memastikan hanya user login yang bisa lihat token
+def home(request):
+    # Logika: Ambil token milik user yang sedang login (request.user)
+    # Jika belum punya token, kita buatkan secara otomatis (get_or_create)
+    token_obj, created = Token.objects.get_or_create(user=request.user)
+    
+    context = {
+        'api_token': token_obj.key,
+        'is_new': created
+    }
+    
+    return render(request, 'index.html', context)   
