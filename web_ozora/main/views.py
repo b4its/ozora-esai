d194064c53d5fef422803e0d93da5446b3b70e7a
b4its <exynosfam@gmail.com>
2026-03-05 10:25:46 +0800

    feat: add views sensor in tools monitoring

--- web_ozora/main/views.py
+++ web_ozora/main/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from rest_framework.authtoken.models import Token
+from iot_service.models import SensorData
 
 @login_required(login_url='customerLogin')
 def home(request):
     # get_or_create mengembalikan tuple: (objek, boolean_apakah_baru)
     token_obj, created = Token.objects.get_or_create(user=request.user)
+    sensorData = SensorData.objects.filter(user=request.user)
     
     # Kita tidak butuh pengecekan 'if token_obj is None' karena 
     # get_or_create pasti mengembalikan objek token atau raise error.
     
     context = {
         'api_token': token_obj.key,
+        'dataSensor': sensorData,
         'is_new': created
     }
     
     return render(request, 'index.html', context)
+
